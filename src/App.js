547ef136160d62602e16d02fd7079aa73ebd439a
ifedayo <ifedayyo12@gmail.com>
2024-07-11 15:33:23 +0100

    this is a currency conversion app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [amount, setAmount] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [convertedAmount, setConvertedAmount] = useState(null);
+  const [currencyData, setCurrencyData] = useState(null);
+
+  function handleInput(event) {
+    setAmount(Number(event.target.value));
+  }
+
+  function handleFromCurrencyChange(event) {
+    setFromCurrency(event.target.value);
+  }
+
+  function handleToCurrencyChange(event) {
+    setToCurrency(event.target.value);
+  }
+
+  function handleConvert() {
+    if (currencyData && amount && fromCurrency && toCurrency) {
+      const fromRate = currencyData[fromCurrency];
+      const toRate = currencyData[toCurrency];
+
+      if (typeof fromRate === "number" && typeof toRate === "number") {
+        const converted = (amount / fromRate) * toRate;
+        setConvertedAmount(converted.toFixed(0));
+      } else {
+        console.log(
+          `Invalid rates => From rate: ${fromRate}, To rate: ${toRate}`
+        );
+      }
+    }
+  }
+
+  useEffect(function () {
+    async function convertCurrency() {
+      try {
+        const res = await fetch(
+          `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_o5DbnBszGqxplDIrE6h2Setm1KEoSR0nuTobjKaq`
+        );
+
+        if (!res.ok)
+          throw new Error(
+            "Something went wrong with fetching currency derivative"
+          );
+
+        const data = await res.json();
+
+        //check if the API response indicates a failure
+        if (data.Response === "False") throw new Error("Currency not found");
+
+        setCurrencyData(data); //update state with fetched data
+        setLoading(false); //set loading to false
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          setError(err.message);
+          setLoading(false);
+        }
+      }
+    }
+    convertCurrency();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="btn-style">
+      <input
+        value={amount}
+        placeholder="Enter amount..."
+        onChange={handleInput}
+        className="btn-style"
+        type="number"
+      />
+
+      <select
+        className="btn-style"
+        value={fromCurrency}
+        onChange={handleFromCurrencyChange}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        className="btn-style"
+        value={toCurrency}
+        onChange={handleToCurrencyChange}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <button className="btn-style" onClick={handleConvert}>
+        Convert
+      </button>
+      {convertedAmount !== null && (
+        <p className="btn-style">converted amount: {convertedAmount}</p>
+      )}
     </div>
   );
 }
-
-export default App;
+//currecncy conversion app. so we enter any number in the
+//input section. afterwards, there will be the a conversion
+//from the first currency to the second one you choose
+//first we want react to recognise our input
